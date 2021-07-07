37c86503916801cdf718408e8f0da546f8af334b
Sandeep Dwivedi <40598729+mail2sandeepd@users.noreply.github.com>
2021-07-07 15:27:15 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,15 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 
+// Get IP address and print
 $host= gethostname();
 $ip = gethostbyname($host);
 
 echo "Server IP Address is: $ip " . "\n";
+
+ 
+// Get Hostname and print
+
+echo gethostname() . "\n";
+echo 'User IP Address - '. $_SERVER['REMOTE_ADDR'] . "\n";
 ?>
